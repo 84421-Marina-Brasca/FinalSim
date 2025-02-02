1e59158767aa4a3f01e9395ea25e4ff7772b1d05
84421-Marina-Brasca <81325595+84421-Marina-Brasca@users.noreply.github.com>
2025-02-02 05:20:54 -0300

    Back

---
+++ Ej_22_Final_BrascaMarina/Entidades/CSVReader.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.Common;
+
+namespace TP5.Entidades
+{
+    internal class CsvReader
+    {
+        StreamReader streamReader;
+
+        public CsvReader(string filePath)
+        {
+            // Instancia el objeto que nos permite escribir en el archivo CSV
+            streamReader = new StreamReader(filePath);
+        }
+
+        private static double Truncar(double nro) => Math.Truncate(nro * 10000) / 10000;
+
+        public double[]? LoadCsvData(DataTable dt)
+        {
+            // Carga con los numeros contenidos en el CSV una tabla, un array (frecObs) con las frecuencias
+            // observadas de cada intervalo y otro array (arrayLimSup) con los limites superiores de cada intervalo
+
+            try
+            {
+                // Crea la cabecera de la tabla
+                dt.Columns.Add("Iteracion");
+                // AGREGAR TODOS LOS OTROS CABEZALES IGUAL A ESTE!!!
+
+                var lineList = new List<string>();
+
+                // Inicializamos las estadísticas para evitar errores
+
+                #region Valores de Calculo
+                lineList[19]= "0"; // T Espera AC Basket
+                lineList[20]= "0"; // T Espera AC Futbol
+                lineList[21] = "0"; // T Espera AC Handball
+                lineList[22] = "0"; // Contador EsperaFinalizada Basket
+                lineList[23] = "0"; // Contador EsperaFinalizada Futbol
+                lineList[24] = "0"; // Contador EsperaFinalizada Handball
+                lineList[25] = "0"; // Cont Llegadas
+                lineList[26] = "0"; // Cont Retirados sin Jugar
+                #endregion
+
+                using (streamReader)
+                {
+                    string? currentLine;
+                    // Leer las líneas del archivo y cargar la tabla de datos
+                    while ((currentLine = streamReader.ReadLine()) != null)
+                    {
+                        lineList = currentLine.Split(';').ToList();
+                        DataRow lineRow = dt.NewRow();
+
+                        lineRow["Iteracion"] = lineList[0];
+                        // AGREGAR TODOS LOS OTROS CABEZALES IGUAL A ESTE!!!
+
+                        dt.Rows.Add(lineRow);
+                    }
+                }
+
+                // Se calcula las probabilidades
+                var iteracionesTotales = double.Parse(lineList[0]);
+
+                // AGREGAR INDICES CORRECTOS A LAS SIGUIENTES LINEAS
+                // var contadorMenosDeLimiteAB = double.Parse(lineList[...]);
+                // var contadorMenosDeLimiteM = double.Parse(lineList[...]);
+                var contadorMenosDeLimiteAB = 0.0;
+                var contadorMenosDeLimiteM = 0.0;
+
+                var probabilidadAB = Truncar(contadorMenosDeLimiteAB / iteracionesTotales);
+                var probabilidadM = Truncar(contadorMenosDeLimiteM / iteracionesTotales);
+
+                return new double[] { probabilidadAB, probabilidadM };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fallo al cargar datos del CSV: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+    }
+}
---
+++ Ej_22_Final_BrascaMarina/Entidades/GestorSimulacion.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5.Entidades
+{
+    internal class GestorSimulacion
+    {
+        public string Datos = @"./datos.csv";
+
+        int IteracionesTotales;
+
+        Random RndA;
+        Random RndB;
+        Random RndC;
+        Random RndD;
+
+        int DuracionM;
+        int DuracionLimite;
+
+        double[] ProbabilidadesA;
+        int[] TiemposA;
+
+        double[] ProbabilidadesB;
+        int[] TiemposB;
+
+        double[] ProbabilidadesC;
+        int[] TiemposC;
+
+        double[] ProbabilidadesD;
+        int[] TiemposD;
+
+        public GestorSimulacion(double inicioImp, int Cantidad, double FinSim, double limsLlegFutbol, double[] limsLlegHandball,
+            double[] limsLlegBasket, double[] limsOcupFutbol, double[] limsOcupHandball, double[] limsOcupBasket,
+            double h, double baskD, double futD, double handD)
+        {
+            RndA = new Random();
+            RndB = new Random();
+            RndC = new Random();
+            RndD = new Random();
+
+            // DuracionM = duracionM;
+            DuracionM = 13;
+
+            // DuracionLimite = duracionLimite;
+            DuracionLimite = 33;
+
+            //ProbabilidadesA = probabilidadesA;
+            ProbabilidadesA = new double[] { 0.25, 0.35, 0.25, 0.15 };
+            TiemposA = new int[] { 5, 6, 7 };
+
+            ProbabilidadesB = new double[] { 0.2, 0.55, 0.25 };
+            TiemposB = new int[] { 3, 5, 7 };
+
+            ProbabilidadesC = new double[] { 0.1, 0.25, 0.4, 0.2, 0.5 };
+            TiemposC = new int[] { 10, 12, 14, 16, 18 };
+
+            ProbabilidadesD = new double[] { 0.6, 0.4 };
+            TiemposD = new int[] { 8, 10 };
+        }
+
+        #region CALCULAR DURACIONES
+        int CalcularDuracionA(double rnd)
+        {
+            if (rnd < ProbabilidadesA[0])
+                return TiemposA[0];
+
+            if (rnd < ProbabilidadesA[0] + ProbabilidadesA[1])
+                return TiemposA[1];
+
+            if (rnd < ProbabilidadesA[0] + ProbabilidadesA[1] + ProbabilidadesA[2])
+                return TiemposA[2];
+
+            return TiemposA[3];
+        }
+
+        int CalcularDuracionB(double rnd)
+        {
+            if (rnd < ProbabilidadesB[0])
+                return TiemposB[0];
+
+            if (rnd < ProbabilidadesB[0] + ProbabilidadesB[1])
+                return TiemposB[1];
+
+            return TiemposB[2];
+        }
+
+        int CalcularDuracionC(double rnd)
+        {
+            if (rnd < ProbabilidadesC[0])
+                return TiemposC[0];
+
+            if (rnd < ProbabilidadesC[0] + ProbabilidadesC[1])
+                return TiemposC[1];
+
+            if (rnd < ProbabilidadesC[0] + ProbabilidadesC[1] + ProbabilidadesC[2])
+                return TiemposC[2];
+
+            if (rnd < ProbabilidadesC[0] + ProbabilidadesC[1] + ProbabilidadesC[2] + ProbabilidadesC[3])
+                return TiemposC[3];
+
+            return TiemposB[4];
+        }
+
+        int CalcularDuracionD(double rnd)
+        {
+            if (rnd < ProbabilidadesD[0])
+                return TiemposD[0];
+
+            return TiemposD[1];
+        }
+        #endregion
+
+        public void Simular()
+        {
+            // Para escribir archivo CSV
+            StreamWriter CSVWriter = new StreamWriter(Datos);
+
+            var contadorConvieneM = 0;
+            var contadorMenosDeLimiteAB = 0;
+            var contadorMenosDeLimiteM = 0;
+
+            for (int i = 0; i < IteracionesTotales; i++)
+            {
+                var valorRndA = RndA.NextDouble();
+                var durA = CalcularDuracionA(valorRndA);
+
+                var valorRndB = RndB.NextDouble();
+                var durB = CalcularDuracionB(valorRndB);
+
+                var valorRndC = RndC.NextDouble();
+                var durC = CalcularDuracionC(valorRndC);
+
+                var valorRndD = RndD.NextDouble();
+                var durD = CalcularDuracionD(valorRndD);
+
+                if (durA + durB > DuracionM)
+                    contadorConvieneM++;
+
+                var durTotalAB = durA + durB + durC + durD;
+                if (durTotalAB < DuracionLimite)
+                    contadorMenosDeLimiteAB++;
+
+                var durTotalM = DuracionM + +durC + durD;
+                if (durTotalAB < DuracionLimite)
+                    contadorMenosDeLimiteM++;
+
+                // Escribir linea CSV...
+                // , RNDs, , M, AB vs M, ...
+                var builder = new StringBuilder().AppendJoin(';',               //SEPARADOR CSV
+                    i,                                                          //Nro Iteracion
+
+                    valorRndA, valorRndB, valorRndC, valorRndD,                 // RNDs
+                    
+                    durA, durB, durC, durD, DuracionM,                          // Duraciones
+                    
+                    durA + durB > DuracionM ? "Si" : "No", contadorConvieneM,   //AB vs M
+                    
+                    durTotalAB,                                                 //ProyectoAB vs Lim
+                    durTotalAB < DuracionLimite ? "Si" : "No",
+                    contadorMenosDeLimiteAB,
+                    
+                    durTotalM,                                                  //ProyectoM vs Lim
+                    durTotalM < DuracionLimite ? "Si" : "No",
+                    contadorMenosDeLimiteM
+                );
+
+                CSVWriter.WriteLine(builder.ToString());
+            }
+
+            CSVWriter.Close();
+        }
+    }
+}
